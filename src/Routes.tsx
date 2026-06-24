2de8bdda02d8f83c8e8a664bd482412f62df9a90
Becca Fiorillo <estevaovrb2014@gmail.com>
2026-06-23 22:56:25 -0300

    troca rotas

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; 
 import Home from "./pages/Home";
 import About from "./pages/About";
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
+    <HashRouter> 
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} /> 
         <Route path="/sobre" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
